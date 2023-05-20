9e3d8aa8b87d19f044608fa19bed9fbf4c791103
pashkov48 <ilya.pashkov48@yandex.ru>
2023-05-20 13:18:52 +0300

    Добавление Language request

---
+++ routes/groups/languages.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\LanguageController;
+
+Route::controller(LanguageController::class)->prefix('v1/languages')->group(function () {
+    Route::get('/', 'list')->name('languages.list');
+});
